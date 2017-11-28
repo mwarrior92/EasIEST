54b44c5f69268fdd418a020a30c1d24e45f00daa
Marc Anthony Warrior <marc.warrior@gmail.com>
2017-11-28 09:55:42 -0600

    added to ripe atlas lib; rearranged package layout; began creating dispatcher; added code that allows top level to select platform specific code by platform name string

--- mms/collector.py
+++ mms/collector.py
@@ -1,13 +1,14 @@
 import threading
 from time import sleep
 from ..helpers import mydir, format_dirpath, top_dir
+from .. import platform_libs
 import json
 
 with open(top_dir+"config.json", 'r+') as f:
     config_data = json.load(f)
 
 class SpinningCollector:
-    def __init__(self, meas_kwargs, retrieval_func, callback, spin_time=30,
+    def __init__(self, meas_kwargs, platform, retrieval_func=None, callback=None, spin_time=30,
             timeout=90):
         """
 
@@ -17,6 +18,7 @@ class SpinningCollector:
         :param spin_time: time to wait between attempts to retrieve measurement results
         """
         self.meas_kwargs = meas_kwargs
+        self.platform = platform
         self.retrieval_func = retrieval_func
         self.result_obtained = False
         self.callback = callback
@@ -29,6 +31,16 @@ class SpinningCollector:
         self.grabber_thread = threading.Thread(target=self.get_result)
         self.grabber_thread.daemon = True
         self.grabber_thread.start()
+
+    def set_retrieval_func(self, meas_type):
+        func_name = meas_type+'_retrieval_func'
+        pl = getattr(platform_libs, self.platform)
+        self.retrieval_func = getattr(pl, func_name)
+
+    def set_callback(self, meas_type):
+        func_name = meas_type+'_callback'
+        pl = getattr(platform_libs, self.platform)
+        self.callback = getattr(pl, func_name)
 
     def get_result(self):
         while True:
@@ -45,7 +57,8 @@ class SpinningCollector:
 
 
 class TriggeredCollector:
-    def __init__(self, meas_kwargs, retrieval_func, trigger_event, callback, timeout=None):
+    def __init__(self, meas_kwargs, platform, trigger_event, retrieval_func=None,
+                 callback=None, timeout=None):
         """
 
         :param meas_kwargs: (dict) will be used as input for retrieval_func
@@ -63,9 +76,20 @@ class TriggeredCollector:
         self.result_obtained = False
         self.timeout = timeout
         self.filename = None
+        self.platform = platform
         self.grabber_thread = threading.Thread(target=self.get_result)
         self.grabber_thread.daemon = True
         self.grabber_thread.start()
+
+    def set_retrieval_func(self, meas_type):
+        func_name = meas_type+'_retrieval_func'
+        pl = getattr(platform_libs, self.platform)
+        self.retrieval_func = getattr(pl, func_name)
+
+    def set_callback(self, meas_type):
+        func_name = meas_type+'_callback'
+        pl = getattr(platform_libs, self.platform)
+        self.callback = getattr(pl, func_name)
 
     def get_result(self):
         self.trigger_event.wait(self.timeout)
